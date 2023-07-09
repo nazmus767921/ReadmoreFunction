549fe68a719db4aa03064fbdd2cb77033de23cfa
Nazmus Sakib <nazmus767921@gmail.com>
2023-07-09 22:49:57 +0600

    Rearranged lines for better visual#

--- readmore.jsx
+++ readmore.jsx
@@ -1,14 +1,17 @@
 //! Created by Nazmus Sakib, GitHub:nazmus767921
 //? Use Better Comments vs code extension to see the comments pop.
+//! ------------------------------------------
 
 //! CALLING SYNTAX
 //? to use the component, first import the component where you want to use it.
 //? Then, call the component function where would be the content and paste the text in -- content=  -- or link the dynamic content.
 // see examples in example.jsx
 <ReadMore content="" />;
+//? ------------------------------------------
 
 //! Imports
 import { useState } from "react";
+//? ------------------------------------------
 
 //! Readmore function
 export const ReadMore = (props) => {
