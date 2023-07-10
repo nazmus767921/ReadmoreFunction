1ca7a2fc7d1b061a9b07ab25a97557c8368acc9d
Nazmus Sakib <nazmus767921@gmail.com>
2023-07-10 13:58:52 +0600

    fixed tailwind issue and removed it

--- readmore.jsx
+++ readmore.jsx
@@ -1,35 +1,37 @@
-//! Created by Nazmus Sakib, GitHub:nazmus767921
-//? Use Better Comments vs code extension to see the comments pop.
+//! Created by Nazmus Sakib, GitHub:nazmus767921 //! USING ONLY REACT
+//? Use Better Comments vs_code extension to see the comments pop.
 //! ------------------------------------------
 
-//! CALLING SYNTAX
+//* CALLING SYNTAX
 //? to use the component, first import the component where you want to use it.
 //? Then, call the component function where would be the content and paste the text in -- content=  -- or link the dynamic content.
 // see examples in example.jsx
-<ReadMore content="" />;
+// <ReadMore content="" />;
 //? ------------------------------------------
 
-//! Imports
+//* Imports
 import { useState } from "react";
-//? ------------------------------------------
 
-//! Readmore function
+//* Readmore function
 export const ReadMore = (props) => {
 	//! props Destructuring
 	const { content } = props;
 
 	//! Default function
-	const [Read, setRead] = useState("line-clamp-4");
+	// ----------------------------------
+	const [Read, setRead] = useState(true);
 	const handleReadmore = () => {
-		if (Read === "line-clamp-4") {
-			setRead("line-clamp-0");
-		} else if (Read === "line-clamp-0") {
-			setRead("line-clamp-4");
-		}
+		setRead((prev) => !prev);
+	};
+	const pStyle = {
+		overflow: "hidden",
+		display: "-webkit-box",
+		WebkitBoxOrient: "vertical",
+		WebkitLineClamp: "4",
 	};
 	return (
 		<>
-			<p className={content.length > 200 ? Read : ""}>{content}</p>
+			<p style={Read ? pStyle : null}>{content}</p>
 			<button
 				onClick={() => handleReadmore()}
 				style={{
@@ -40,8 +42,36 @@ export const ReadMore = (props) => {
 					backgroundColor: "#000",
 					borderRadius: "5px",
 				}}>
-				readmore
+				{Read ? "Read More" : "Read Less"}
 			</button>
 		</>
 	);
 };
+
+// //* Readmore function
+// export const ReadMore = (props) => {
+// 	//* props Destructuring
+// 	const { content } = props;
+
+// 	//* Default function
+// 	const [Read, setRead] = useState("line-clamp-4");
+// 	const handleReadmore = () => {
+// 		if (Read === "line-clamp-4") {
+// 			setRead("");
+// 		} else if (Read === "") {
+// 			setRead("line-clamp-4");
+// 		}
+// 	};
+// 	return (
+// 		<>
+// 			<p className={content.length > 200 ? Read : ""}>{content}</p>
+// 			<button
+// 				onClick={() => handleReadmore()}
+// 				className={
+// 					content.length > 200 ? "flex btn py-1 px-2 text-xl my-6" : "hidden"
+// 				}>
+// 				readmore
+// 			</button>
+// 		</>
+// 	);
+// };
